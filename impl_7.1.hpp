ee0b20783750bec36bd3dc957135b5e602af3ef8
Marius Fabry <fabry.marius@gmail.com>
2020-04-07 23:15:02 +0200

    Implementation of problem 7.1

--- impl_7.1.hpp
+++ impl_7.1.hpp
@@ -1,18 +1,47 @@
 #pragma one
 
 #include <string>
+#include <algorithm>
 #include <stdint.h>
 
 std::string integer_to_string(int64_t value)
 {
     std::string result;
-    // TODO: implement
+    bool is_negative = false;
+    if (value < 0) {
+        is_negative = true;
+        value = -value;
+    }
+    while (value) {
+        const auto digit = value % 10;
+        result.push_back(digit + '0');
+        value /= 10;
+    }
+    if (is_negative) {
+        result.push_back('-');
+    }
+    std::reverse(result.begin(), result.end());
     return result;
 }
 
 int64_t string_to_integer(const std::string& string)
 {
     int64_t result = 0;
-    // TODO: implement
+    if (string.length() > 0) {
+        bool is_negative = false;
+        if (string[0] == '-') {
+            is_negative = true;
+        }
+        for (auto str_idx = (is_negative ? 1 : 0); str_idx < string.length(); ++str_idx) {
+            const auto digit = string[str_idx] - '0';
+            if (digit < 0 || digit > 9) {
+                return 0;
+            }
+            result = result * 10 + digit;
+        }
+        if (is_negative) {
+            result = -result;
+        }
+    }
     return result;
 }
--- tests_7.1.cpp
+++ tests_7.1.cpp
@@ -3,7 +3,7 @@
 
 class Problem7_1 {
 public:
-    static void Problem7_1::run_tests()
+    static void run_tests()
     {
         test_positive_number_int_to_string();
         test_negative_number_int_to_string();
@@ -11,22 +11,22 @@ public:
         test_negative_number_string_to_int();
     }
 
-    static void Problem7_1::test_positive_number_int_to_string()
+    static void test_positive_number_int_to_string()
     {
         TEST_ASSERT_EQUAL(integer_to_string(123), "123");
     }
 
-    static void Problem7_1::test_negative_number_int_to_string()
+    static void test_negative_number_int_to_string()
     {
         TEST_ASSERT_EQUAL(integer_to_string(-11), "-11");
     }
 
-    static void Problem7_1::test_positive_number_string_to_int()
+    static void test_positive_number_string_to_int()
     {
         TEST_ASSERT_EQUAL(string_to_integer("123"), 123);
     }
 
-    static void Problem7_1::test_negative_number_string_to_int()
+    static void test_negative_number_string_to_int()
     {
         TEST_ASSERT_EQUAL(string_to_integer("-11"), -11);
     }
