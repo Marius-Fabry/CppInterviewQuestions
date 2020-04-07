a349a64b4f0b23721670470c60c6df6f0605d1ec
Marius Fabry <fabry.marius@gmail.com>
2020-04-07 18:19:36 +0200

    Added skeleton code for problem 7.1

---
+++ impl_7.1.hpp
@@ -0,0 +1,18 @@
+#pragma one
+
+#include <string>
+#include <stdint.h>
+
+std::string integer_to_string(int64_t value)
+{
+    std::string result;
+    // TODO: implement
+    return result;
+}
+
+int64_t string_to_integer(const std::string& string)
+{
+    int64_t result = 0;
+    // TODO: implement
+    return result;
+}
--- tests_7.1.cpp
+++ tests_7.1.cpp
@@ -1,3 +1,51 @@
-int main() {
-	return 0;
+#include "testing.hpp"
+#include "impl_7.1.hpp"
+
+// ---- Declarations ----
+
+int main();
+
+class Problem7_1 {
+public:
+    static void run_tests();
+    static void test_positive_number_int_to_string();
+    static void test_negative_number_int_to_string();
+    static void test_positive_number_string_to_int();
+    static void test_negative_number_string_to_int();
+};
+
+// ---- Definitions ----
+
+int main()
+{
+    Problem7_1::run_tests();
+    return 0;
+}
+
+void Problem7_1::run_tests()
+{
+    test_positive_number_int_to_string();
+    test_negative_number_int_to_string();
+    test_positive_number_string_to_int();
+    test_negative_number_string_to_int();
+}
+
+void Problem7_1::test_positive_number_int_to_string()
+{
+    TEST_ASSERT_EQUAL(integer_to_string(123), "123");
+}
+
+void Problem7_1::test_negative_number_int_to_string()
+{
+    TEST_ASSERT_EQUAL(integer_to_string(-11), "-11");
+}
+
+void Problem7_1::test_positive_number_string_to_int()
+{
+    TEST_ASSERT_EQUAL(string_to_integer("123"), 123);
+}
+
+void Problem7_1::test_negative_number_string_to_int()
+{
+    TEST_ASSERT_EQUAL(string_to_integer("-11"), -11);
 }
