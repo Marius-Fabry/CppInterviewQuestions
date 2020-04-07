0c29401158a1fd39fc9ee116f1ca4f754cc55349
Marius Fabry <fabry.marius@gmail.com>
2020-04-07 12:53:05 +0200

    Commented out the fail-on-purpose test from tests_0.cpp for continuous integration.

--- tests_0.cpp
+++ tests_0.cpp
@@ -4,22 +4,29 @@
 constexpr int SUCCESS = 0;
 constexpr int FAILURE = 1;
 
-#define ASSERT(condition)                                                      \
-  if (!(condition)) {                                                          \
-    std::cerr << __func__ << " / line " << __LINE__ << std::endl;              \
-    exit(FAILURE);                                                             \
-  }
+#define ASSERT(condition)                                             \
+    if (!(condition)) {                                               \
+        std::cerr << __func__ << " / line " << __LINE__ << std::endl; \
+        exit(FAILURE);                                                \
+    }
 
 int main();
 void test_1();
 void test_2();
 
-int main() {
-  test_1();
-  test_2();
-  return SUCCESS;
+int main()
+{
+    test_1();
+    test_2();
+    return SUCCESS;
 }
 
 void test_1() { ASSERT(true); }
 
-void test_2() { ASSERT(false); }
+void test_2()
+{
+    ASSERT(true);
+    // Failing removed so that the continuous integration
+    // tests don't fail.
+    // ASSERT(false);
+}
